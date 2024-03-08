c82e801bcafc7037f08bc87e6089fa7ae7c08351
Oscar Contreras <81644152+raidolContreras@users.noreply.github.com>
2024-03-08 11:50:04 -0600

    Update eventoActivo.php

--- view/pages/eventoActivo.php
+++ view/pages/eventoActivo.php
@@ -4,8 +4,6 @@
             <strong id="evento"></strong>
         </div>
     </div>
-
-    <button class="btn btn-primary mb-3 float-end" data-bs-toggle="modal" data-bs-target="#invitadosModal">Registrar invitados</button>
 	
 	<div>
 		<table id="tableEvents" class="table table-resposive stripe">
@@ -21,25 +19,5 @@
 		</table>
 	</div>
 </main>
-<input type="hidden" name="idEvent">
-<!-- Modal para el registro de eventos -->
-<div class="modal fade" id="invitadosModal" tabindex="-1" aria-labelledby="invitadosModalLabel" aria-hidden="true">
-	<div class="modal-dialog">
-		<div class="modal-content">
-			<div class="modal-body">
-				<center>
-					<a class="btn-custom btn-editar mb-4" href="view/assets/docs/Plantilla de invitaciones a eventos.csv" download='Plantilla de invitaciones a eventos.csv' >Descargar plantilla</a>
-				</center>
-				<form class="registerInv">
-					<!-- Agrega un elemento div con el id myDropzone para Dropzone -->
-					<div id="myDropzone" class="dropzone"></div>
-				</form>
-				<div class="center-buttons mt-4">
-					<button type="submit" class="btn btn-primary mx-1" id="register" data-bs-dismiss="modal">Registrar</button>
-					<button type="button" class="btn btn-danger mx-1" data-bs-dismiss="modal">Cancelar</button>
-				</div>
-			</div>
-		</div>
-	</div>
-</div>
-<script src="view\assets\js\events\getEventActive.js"></script>
+<input type="hidden" name="idEvent" value="<?php echo $_GET['event'] ?>">
+<script src="view\assets\js\events\getInvitados.js"></script>
